6578f6884978ec7394d305ef7e16a9c9bf9bee2d
EvgenyKatukhov <ekatuhov@gmail.com>
2022-02-18 12:56:31 +0300

    calculate

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@ public class Main {
     public static void main(String[] args) {
         int eaters = 5; // сколько людей будут есть
         int water = 3000; // миллилитров воды
-        int potatoes = 5; // картофелин
-        int chicken = 6; // куриных бёдер
+        int chicken = 6; // куриных бедер
+        int potatoes = 5; // картофели
         int spices = 10; // ложек специй
         System.out.println("Сварили суп. На одного человека вышло:");
         System.out.println((water / eaters) + " миллилитров воды");
